ba0d9d2ff6333d0c2f652161799022f73cc8dde3
JP Carrascal <juanpcarrascal@gmail.com>
2023-12-28 08:59:59 -0500

    First code commit

---
+++ index.js
@@ -0,0 +1,53 @@
+// Import necessary modules
+const express = require('express');
+const bodyParser = require('body-parser');
+var QRCode = require('qrcode');
+
+// Initialize express app
+const app = express();
+
+// Use body-parser middleware to handle JSON payloads
+app.use(bodyParser.json());
+
+// Define a simple GET endpoint
+app.get('/qr', (req, res) => {
+    if(req.query.string) {
+        let width = 400;
+        if(req.query.width) width = req.query.width;
+        var string = req.query.string;
+        const options = { errorCorrectionLevel: 'H',
+                        type: 'image/png',
+                        quality: 0.3,
+                        width: width,
+                        margin: 1};
+        QRCode.toDataURL(string, options, function (err, base64Str) {
+            var imgUri = base64Str.split(';base64,').pop()
+            var img = Buffer.from(imgUri, 'base64');
+            res.writeHead(200, {
+                'Content-Type': 'image/png',
+                'Content-Length': img.length
+              });
+              res.end(img);
+        });
+    } else {
+        res.json({ message: 'No string provided!' });
+    }
+},function(err){
+    res.err(err);
+    console.log(err);
+});
+
+// Define a simple POST endpoint
+app.post('/', (req, res) => {
+    // Log the request body
+    console.log(req.body);
+
+    // Respond with a success message
+    res.json({ message: 'Data received!' });
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
